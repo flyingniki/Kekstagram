e1ed410847b51aa5b071627dd8bee39c6f6ad6ae
Petr Nikitin <flying_niki@mail.ru>
2022-05-16 19:10:37 +0300

    использована модуляция

---
+++ js/util.js
@@ -0,0 +1,22 @@
+function getRandomInt(min, max) {
+  if (min >= 0 && max >= 0) {
+    if (max < min) {
+      [min, max] = [max, min];
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return null;
+}
+
+function strMaxLenght(string, maxLength) {
+  return string.length <= maxLength ? true : false;
+}
+
+strMaxLenght('Test string', 140);
+
+function getRandomElement(array) {
+  let index = getRandomInt(0, array.length - 1);
+  return array[index];
+}
+
+export { getRandomInt, getRandomElement };
