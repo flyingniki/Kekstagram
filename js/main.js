8afcb66139ecf381606293c144bc51d4ceb5c7ae
Petr Nikitin <flying_niki@mail.ru>
2022-05-18 18:00:34 +0300

    доработан main

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { addPhotos } from './data.js';
 import { addPreviewPictures } from './preview.js';
 
 let photos = [];
-let previewPictures = [];
 
 photos = addPhotos();
-previewPictures = addPreviewPictures();
+
+addPreviewPictures();
