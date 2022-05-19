b978f63728e94a570b1c474b20aa3184f402ef9f
Petr Nikitin <flying_niki@mail.ru>
2022-05-19 19:25:22 +0300

    добавлен вывод большой картинки

---
+++ js/fullpicture.js
@@ -0,0 +1,48 @@
+const picture = document.querySelector('.big-picture');
+const pictureImg = document.querySelector('.big-picture__img > img');
+const pictureLikes = document.querySelector('.likes-count');
+const pictureCommentsCount = document.querySelector('.comments-count');
+
+const pictureCommentsList = document.querySelector('.social__comments');
+const pictureComment = document.querySelector('.social__comment');
+const pictureCommentsFragment = document.createDocumentFragment();
+
+const pictureDescription = document.querySelector('.social__caption');
+
+const buttonClose = document.querySelector('.big-picture__cancel');
+
+function showPicture(url, comments, likes, description) {
+  picture.classList.remove('hidden');
+  document.querySelector('.social__comment-count').classList.add('hidden');
+  document.querySelector('.comments-loader').classList.add('hidden');
+  document.querySelector('body').classList.add('modal-open');
+
+  pictureImg.src = url;
+  pictureLikes.textContent = likes;
+  pictureCommentsCount.textContent = comments.length;
+  pictureDescription.textContent = description;
+
+  comments.forEach((comment) => {
+    const commentElement = pictureComment.cloneNode(true);
+
+    commentElement.querySelector('.social__picture').src = comment.avatar;
+    commentElement.querySelector('.social__picture').alt = comment.name;
+    commentElement.querySelector('.social__text').textContent = comment.message;
+    pictureCommentsFragment.appendChild(commentElement);
+  });
+
+  pictureCommentsList.appendChild(pictureCommentsFragment);
+}
+
+buttonClose.addEventListener('click', () => {
+  picture.classList.add('hidden');
+});
+
+document.addEventListener('keydown', (evt) => {
+  const keyName = evt.key;
+  if (keyName === 'Escape') {
+    picture.classList.add('hidden');
+  }
+});
+
+export { showPicture };
--- js/preview.js
+++ js/preview.js
@@ -1,4 +1,5 @@
 import { addPhotos } from './data.js';
+import { showPicture } from './fullpicture.js';
 
 let photos = addPhotos();
 
@@ -9,12 +10,16 @@ const previewPictures = document.querySelector('.pictures');
 
 const previewFragment = document.createDocumentFragment();
 
-photos.forEach(({ url, comments, likes }) => {
+photos.forEach(({ url, comments, likes, description }) => {
   const previewElement = previewTemplate.cloneNode(true);
   previewElement.querySelector('.picture__img').src = url;
   previewElement.querySelector('.picture__comments').textContent =
     comments.length;
   previewElement.querySelector('.picture__likes').textContent = likes;
+  previewElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showPicture(url, comments, likes, description);
+  });
   previewFragment.appendChild(previewElement);
 });
 
